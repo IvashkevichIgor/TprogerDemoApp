c450a07cac57ea41fb0cad9105b05a7ffda9e474
Igor Ivashkevich <ivashkevich.java@gmail.com>
2022-11-21 14:54:31 +0300

    initial commit

---
+++ src/main/java/ru/ivashkevich/tproger_demo_app/TprogerDemoAppApplication.java
@@ -0,0 +1,13 @@
+package ru.ivashkevich.tproger_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TprogerDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TprogerDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ivashkevich/tproger_demo_app/TprogerDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ivashkevich.tproger_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TprogerDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
