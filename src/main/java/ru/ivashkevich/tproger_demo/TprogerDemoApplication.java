4cebbc81ace0bfc1e571ddc17856aa37375b3da9
Igor Ivashkevich <ivashkevich.java@gmail.com>
2022-11-21 15:42:01 +0300

    add database connection, HelloConnector for simple test

--- src/main/java/ru/ivashkevich/tproger_demo/TprogerDemoApplication.java
+++ src/main/java/ru/ivashkevich/tproger_demo/TprogerDemoApplication.java
@@ -1,13 +1,13 @@
-package ru.ivashkevich.tproger_demo_app;
+package ru.ivashkevich.tproger_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TprogerDemoAppApplication {
+public class TprogerDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TprogerDemoAppApplication.class, args);
+        SpringApplication.run(TprogerDemoApplication.class, args);
     }
 
 }
--- src/test/java/ru/ivashkevich/tproger_demo/TprogerDemoApplicationTests.java
+++ src/test/java/ru/ivashkevich/tproger_demo/TprogerDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package ru.ivashkevich.tproger_demo_app;
+package ru.ivashkevich.tproger_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TprogerDemoAppApplicationTests {
+class TprogerDemoApplicationTests {
 
     @Test
     void contextLoads() {
